b26b83aefcf3ebfd1b4e9c725d32ef20f691cfc4
Michael Brandon Morris <mmorris@courtcall.com>
2019-03-15 02:03:56 -0400

    Initial commit

---
+++ MichaelBrandonMorris.Entity/Base/Entity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MichaelBrandonMorris.Entity.Base
+{
+    public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    {
+        protected Entity()
+        {
+            if (typeof(TId) != typeof(int) && 
+                typeof(TId) != typeof(long) && 
+                typeof(TId) != typeof(Guid) &&
+                typeof(TId) != typeof(string))
+            {
+                throw new NotSupportedException($"Type {typeof(TId)} is not a supported type for the Id property.");
+            }
+        }
+
+        public TId Id { get; set; }
+
+        /// <inheritdoc />
+        public bool Equals(Entity<TId> other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            return ReferenceEquals(this, other) || EqualityComparer<TId>.Default.Equals(Id, other.Id);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj.GetType() == this.GetType() && Equals((Entity<TId>) obj);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return EqualityComparer<TId>.Default.GetHashCode(Id);
+        }
+
+        public static bool operator ==(Entity<TId> left, Entity<TId> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Entity<TId> left, Entity<TId> right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
---
+++ MichaelBrandonMorris.Entity/GuidEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MichaelBrandonMorris.Entity
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// </summary>
+    public abstract class GuidEntity : IEquatable<GuidEntity>
+    {
+        public Guid Id { get; set; }
+
+        /// <inheritdoc />
+        public bool Equals(GuidEntity other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            return ReferenceEquals(this, other) || Id.Equals(other.Id);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj.GetType() == GetType() && Equals((GuidEntity) obj);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            // ReSharper disable once NonReadonlyMemberInGetHashCode
+            return Id.GetHashCode();
+        }
+    }
+}
